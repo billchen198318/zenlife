2297818fa75ba28917c796b7ab4d00ddf3801bdb
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-11-08 15:32:15 +0800

    update

---
+++ zenlife-standard/src/com/zenlife/base/interceptor/ZenLifeLoginHandlerInterceptor.java
@@ -0,0 +1,58 @@
+/* 
+ * Copyright 2012-2017 qifu of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package com.zenlife.base.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.qifu.base.Constants;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ZenLifeLoginHandlerInterceptor implements HandlerInterceptor {
+	
+	public ZenLifeLoginHandlerInterceptor() {
+		super();
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelView) throws Exception {
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		Object accountId = request.getSession().getAttribute(Constants.SESS_ACCOUNT);
+		if (accountId != null && SecurityUtils.getSubject().isAuthenticated()) {
+			return true;
+		}
+		response.sendRedirect("logout.do");
+		return false;
+	}
+	
+}
